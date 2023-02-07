7867d5ce8554a8bbd582a5809197b5f91a7dc3c5
sadamseehar <sadamseehar@gmail.com>
2023-02-08 02:23:36 +0500

    adding functionality to product quantity increment and decrement buttons

---
+++ static/js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function (){
+
+
+    $('.increment-btn').click(function(e){
+
+        e.preventDefault();
+        var inc_value = $(this).closest('.product_data').find('.qty-input').val();
+        var value = parseInt(inc_value,10);
+        value = isNaN(value) ? 0 : value;
+        if(value < 10)
+        {
+            value++;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+
+    
+    $('.decrement-btn').click(function(e){
+
+        e.preventDefault();
+        var dec_value = $(this).closest('.product_data').find('.qty-input').val();
+        var value = parseInt(dec_value,10);
+        value = isNaN(value) ? 0 : value;
+        if(value > 1)
+        {
+            value--;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+});
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
 # Create your views here.
